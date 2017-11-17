524dde4f9521c9104a7fb3f630fcebafa99e1d1d
conorhalloran <conorhalloran@gmail.com>
2017-11-17 14:00:12 -0800

    Add accepts_nested_attributes_for :survey_answers

--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -1,5 +1,6 @@
 class SurveyQuestion < ApplicationRecord
   belongs_to :user
   has_many :survey_answers, dependent: :destroy
+  accepts_nested_attributes_for :survey_answers
   validates :body, presence: true
 end
